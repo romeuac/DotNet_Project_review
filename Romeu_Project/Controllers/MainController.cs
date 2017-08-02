5a53d0854c6e4190512fcd7866851488c8a07a29
Romeu_Visual_Studio_2017 <romeuac@gmail.com>
2017-08-02 17:45:34 -0300

    Standard version for projects in dotnet

---
+++ Romeu_Project/Controllers/MainController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Romeu_Project.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.value = "My First MVC!!!";
+            return View();
+        }
+    }
+}
---
+++ Romeu_Project/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Romeu_Project
+{
+    public class Program
+    {
+        // Eh a ENTRADA da aplicacao
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                // Permitimos que use Kestrel como servidor a ser usado como o web host
+                .UseKestrel()
+                // Usa o caminho de conteúdo do Root pegando o diretorio atual da aplicacao
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                // Usando integracao IIS 
+                .UseIISIntegration()
+                // Usa o Startup que configura-se no outro arquivo Startup.cs
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                // Build a aplicacao
+                .Build();
+
+            host.Run();
+        }
+    }
+}
