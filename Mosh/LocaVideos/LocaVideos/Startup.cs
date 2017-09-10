31a89dc0c83da563e611f48f4eb06cb2b4219735
Romeu Andrade Cavalcante <romeuac@gmail.com>
2017-09-10 17:24:19 -0300

    Inicial

---
+++ Mosh/LocaVideos/LocaVideos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LocaVideos.Startup))]
+namespace LocaVideos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Mosh/Locadora/Locadora/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Locadora.Startup))]
+namespace Locadora
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
